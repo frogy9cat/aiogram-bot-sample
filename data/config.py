1de61a68567d26daf7964e20d623c152bc39f52b
frogy9cat <109206156+frogy9cat@users.noreply.github.com>
2022-08-18 11:47:39 +0500

    Update config.py

--- data/config.py
+++ data/config.py
@@ -1,10 +1,15 @@
-from environs import Env
+# from environs import Env
 
-# environs kutubxonasidan foydalanish
-env = Env()
-env.read_env()
+# # environs kutubxonasidan foydalanish
+# env = Env()
+# env.read_env()
 
-# .env fayl ichidan quyidagilarni o'qiymiz
-BOT_TOKEN = env.str("BOT_TOKEN")  # Bot toekn
-ADMINS = env.list("ADMINS")  # adminlar ro'yxati
-IP = env.str("ip")  # Xosting ip manzili
+# # .env fayl ichidan quyidagilarni o'qiymiz
+# BOT_TOKEN = env.str("BOT_TOKEN")  # Bot toekn
+# ADMINS = env.list("ADMINS")  # adminlar ro'yxati
+# IP = env.str("ip")  # Xosting ip manzili
+
+
+BOT_TOKEN='5486329124:AAH59_ue7kOLGTGYjxshfbRBKFFggs-0kTA'
+ADMINS=['486178287']
+IP='localhost'
