27d16b285ab243eb266aa9fe158da2ad464c9cd7
Ramazan <109206156+frogy9cat@users.noreply.github.com>
2023-02-01 14:18:34 +0500

    Update config.py

--- data/config.py
+++ data/config.py
@@ -1,15 +1,15 @@
 # from environs import Env
 
-# # environs kutubxonasidan foydalanish
+
 # env = Env()
 # env.read_env()
 
-# # .env fayl ichidan quyidagilarni o'qiymiz
-# BOT_TOKEN = env.str("BOT_TOKEN")  # Bot toekn
-# ADMINS = env.list("ADMINS")  # adminlar ro'yxati
-# IP = env.str("ip")  # Xosting ip manzili
+
+# BOT_TOKEN = env.str("BOT_TOKEN")
+# ADMINS = env.list("ADMINS") 
+# IP = env.str("ip")  
 
 
-BOT_TOKEN='5486329124:AAH59_ue7kOLGTGYjxshfbRBKFFggs-0kTA'
-ADMINS=['486178287']
+BOT_TOKEN='{bot token}'
+ADMINS=['telegram id']
 IP='localhost'
